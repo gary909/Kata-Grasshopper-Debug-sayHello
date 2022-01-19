28fc30f60c48f4210f1b3c4c91a6f1f61a76abfa
gary <gary.white@lodgit.com>
2022-01-19 13:01:27 +0100

    code working

---
+++ index.js
@@ -0,0 +1,6 @@
+function sayHello(name) {
+    return 'Hello, ' + name;
+}
+
+console.log(sayHello('Mr. Spock')); // 'Hello, Mr. Spock'
+console.log(sayHello('Captain Kirk')); // 'Hello, Captain Kirk'
